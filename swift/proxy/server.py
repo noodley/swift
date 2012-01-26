cfd67435cc7a64b968d58a049ced9e2fe7043d93
John Dickinson <me@not.mn>
2012-01-25 19:42:58 -0600

    fixed DELETEs
    Change-Id: If74b85b0ca6fee14c89d2c68363258daa5b3448c

--- swift/proxy/server.py
+++ swift/proxy/server.py
@@ -1374,10 +1374,11 @@ class ObjectController(Controller):
     def DELETE(self, req):
         """HTTP DELETE request handler."""
         # we need to check if this is a versioned object
-        req.method = 'HEAD'
-        obj_head_resp = self.HEAD(req)
-        req.method = 'DELETE'
-        if 'x-object-versions' in obj_head_resp.headers:
+        hreq = Request.blank(req.path_info, headers={'X-Newest': 'True'},
+                             environ={'REQUEST_METHOD': 'HEAD'})
+        obj_head_resp = self.HEAD(hreq)
+        which_version = get_param(req, 'v')
+        if 'x-object-versions' in obj_head_resp.headers and which_version:
             # this is a version manifest and needs to be handled differently
             lcontainer, lprefix = \
                 obj_head_resp.headers['x-object-versions'].split('/', 1)
@@ -1392,25 +1393,8 @@ class ObjectController(Controller):
             except ListingIterNotAuthorized, err:
                 return err.aresp
             # now that we have a list of the versions, pick the right one
-            which_version = get_param(req, 'v')
-            if which_version:
-                # client requested a particular version to delete
-                if which_version == '0':
-                    container_name_to_delete = self.container_name
-                    object_name_to_delete = self.object_name
-                else:
-                    container_name_to_delete = lcontainer
-                    object_name_to_delete = lprefix + which_version
-            elif listing:
-                # no version specified, but we have a listing
-                which_version = listing[-1]['name'][len(lprefix):]
-                container_name_to_delete = lcontainer
-                object_name_to_delete = lprefix + which_version
-            else:
-                container_name_to_delete = self.container_name
-                object_name_to_delete = self.object_name
-            self.container_name = container_name_to_delete
-            self.object_name = object_name_to_delete
+            self.container_name = lcontainer
+            self.object_name = lprefix + which_version
             req.path_info = '/' + self.account_name + '/' + \
                             self.container_name + '/' + self.object_name
         # now that we have the right object reference, proceed as normal
